8b2c82900690cdd0af259200e0c2a8a78b77ff47
Benjamin Duran <benjamind10@pm.me>
2022-04-03 23:12:05 -0400

    added validation

--- models/Pizza.js
+++ models/Pizza.js
@@ -5,46 +5,51 @@ const PizzaSchema = new Schema(
   {
     pizzaName: {
       type: String,
+      required: true,
+      trim: true
     },
     createdBy: {
       type: String,
+      required: true,
+      trim: true
     },
     createdAt: {
       type: Date,
       default: Date.now,
-      get: createdAtVal => dateFormat(createdAtVal),
+      get: createdAtVal => dateFormat(createdAtVal)
     },
     size: {
       type: String,
-      default: 'Large',
+      required: true,
+      enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
+      default: 'Large'
     },
     toppings: [],
     comments: [
       {
         type: Schema.Types.ObjectId,
-        ref: 'Comment',
-      },
-    ],
+        ref: 'Comment'
+      }
+    ]
   },
   {
     toJSON: {
       virtuals: true,
-      getters: true,
+      getters: true
     },
-    id: false,
+    // prevents virtuals from creating duplicate of _id as `id`
+    id: false
   }
 );
 
 // get total count of comments and replies on retrieval
-PizzaSchema.virtual('commentCount').get(function () {
+PizzaSchema.virtual('commentCount').get(function() {
   return this.comments.reduce(
     (total, comment) => total + comment.replies.length + 1,
     0
   );
 });
 
-// create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
 
-// export the Pizza model
 module.exports = Pizza;
